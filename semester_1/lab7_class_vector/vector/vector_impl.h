a34c5e7555d360e90395c9619c1e44ede4caaac0
d7aun <vadimputyrski345@gmail.com>
2025-12-09 22:29:29 +0300

    Update vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,34 @@
 #pragma once
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <initializer_list>
 
 class Vector {
-    // your code goes here
+	int* data;
+	size_t size;
+	size_t cap;
+public:
+	Vector();
+	Vector(size_t xSize);
+
+	//initializer_list
+	Vector(std::initializer_list<int> numbers);
+
+	Vector(const Vector& other);
+	~Vector();
+	bool operator ==(const Vector& other);
+	Vector operator =(const Vector& other);
+	void Swap(Vector& other);
+	int& operator [](size_t i) const;
+	int& operator [](size_t i);
+	int& At(size_t i) const;
+	int& At(size_t i);
+	size_t Size() const;
+	size_t Capacity() const;
+	void PushBack(int a);
+	void PopBack();
+	void Clear();
+	void Reserve(size_t xCap);
 };
+std::ostream& operator<<(std::ostream& out, const Vector& vec);
