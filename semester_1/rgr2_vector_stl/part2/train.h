a67945ad40c89376a52a4226afc4724a2adc5197
d7aun <vadimputyrski345@gmail.com>
2025-12-23 00:47:19 +0300

    train.h emergency fix

--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -15,11 +15,19 @@ enum class TrainType {
 class Train {
 private:
     TrainId id_;
+    //ID would be 1 to 999999
     TrainType type_;
     std::string destination_;
     std::time_t dispatch_time_;
     std::time_t travelling_time_;
 
 public:
-    // your code goes here
+    Train();
+    void readTrainFile(std::vector<Train>& v, const std::string& fileName);
+    void sortByDispTime(std::vector<Train>& v);
+    void displayTrainInfo_timeInterval(std::vector<Train>& v, std::string beginning_, std::string ending_);
+    void displayTrainInfo_destination(std::vector<Train>& v, std::string destination_);
+    void displayTrainInfo_destination_type(std::vector<Train>& v, std::string destination_, TrainType type_);
+    void displayFastestTrainInfo_destination(std::vector<Train>& v, std::string destination_);
+    void myTrain(const Train& tr);
 };
