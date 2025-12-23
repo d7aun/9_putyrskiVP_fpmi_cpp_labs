a588bd08bf1e4e3e4bb7dad3c618fc84a68513e3
d7aun <vadimputyrski345@gmail.com>
2025-12-23 21:33:05 +0300

    Update train.h

--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "time_utility.h"
+#include <iomanip>
 
 using TrainId = size_t;
 using namespace time_utility;
 
 
-enum class TrainType { 
+enum class TrainType {
     PASSENGER, FREIGHT, HIGH_SPEED, SUBWAY, SPECIALIZED
 };
 
@@ -23,11 +27,11 @@ private:
 
 public:
     Train();
-    void readTrainFile(std::vector<Train>& v, const std::string& fileName);
-    void sortByDispTime(std::vector<Train>& v);
-    void displayTrainInfo_timeInterval(std::vector<Train>& v, std::string beginning_, std::string ending_);
-    void displayTrainInfo_destination(std::vector<Train>& v, std::string destination_);
-    void displayTrainInfo_destination_type(std::vector<Train>& v, std::string destination_, TrainType type_);
-    void displayFastestTrainInfo_destination(std::vector<Train>& v, std::string destination_);
-    void myTrain(const Train& tr);
+    friend void readTrainFile(std::vector<Train>& v, const std::string& fileName);
+    friend void sortByDispTime(std::vector<Train>& v);
+    friend void displayTrainInfo_timeInterval(std::vector<Train>& v, std::string beginning_, std::string ending_);
+    friend void displayTrainInfo_destination(std::vector<Train>& v, std::string destination_);
+    friend void displayTrainInfo_destination_type(std::vector<Train>& v, std::string destination_, TrainType type_);
+    friend void displayFastestTrainInfo_destination(std::vector<Train>& v, std::string destination_);
+    void myTrain();
 };
