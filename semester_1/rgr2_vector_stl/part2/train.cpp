ef61c345109aa1852e3f93cd68533e1bf48acd0f
d7aun <vadimputyrski345@gmail.com>
2025-12-23 21:33:49 +0300

    Update train.cpp

--- semester_1/rgr2_vector_stl/part2/train.cpp
+++ semester_1/rgr2_vector_stl/part2/train.cpp
@@ -45,115 +45,16 @@ bool isInt(const std::string& s)
 {
 	if (s.size() > 9)
 		return 0;
-	if (s == "00")
-		return true;
 	int a = stoi(s);
 	std::string reS = std::to_string(a);
-	if (reS != s)
+	int dif = size(s) - size(reS);
+	std::string xreS;
+	for (int i = 0; i < dif; ++i)
+		xreS += '0';
+	xreS += reS;
+	if (xreS != s && reS != s)
 		return false;
 	return true;
-}
-
-Train::Train() {};
-void Train::readTrainFile(std::vector<Train>& v, const std::string& fileName)
-{
-	std::ifstream in;
-	in.open(fileName);
-	if (!in)
-	{
-		throw "Input file error";
-	}
-	std::string info;
-	std::string part;
-	std::string partT;
-	size_t beg, begT;
-	size_t end, endT;
-	int hours, minutes;
-	Train tr;
-	while (!in.eof())
-	{
-		getline(in, info);
-		try
-		{
-			beg = 0;
-			end = info.find_first_of(' ');
-			strTest(beg, end);
-
-			part = info.substr(beg, end - beg);
-			if (!isInt(part))
-				throw "Inappropriate TrainID";
-			if (stoi(part) > 999999)
-				throw "Inappropriate TrainID: limits exceeding";
-			tr.id_ = stoi(part);
-			beg = ++end;
-			end= info.find_first_of(beg, ' ');
-			strTest(beg, end);
-
-
-			part = info.substr(beg, end - beg);
-			tr.type_=strTrainType(part);
-			beg = ++end;
-			end = info.find_first_of(beg, ' ');
-			strTest(beg, end);
-
-
-			part = info.substr(beg, end - beg);
-			tr.destination_ = part;
-			beg = ++end;
-			end = info.find_first_of(beg, ' ');
-			strTest(beg, end);
-
-
-			part = info.substr(beg, end - beg);
-			endT= info.find_first_of(beg, ':');
-			strTest(beg, end);
-			partT= info.substr(beg, endT - beg);
-			if (!isInt(partT))
-				throw "Inappropriate dispatch hours";
-			hours = stoi(partT);
-			if (hours > MAX_HOUR_NUMBER_PER_DAY)
-				throw "Inappropriate dispatch hours";
-
-			begT = ++endT;
-			strTest(beg, end);
-			partT = info.substr(begT, end - begT);
-			if (!isInt(partT))
-				throw "Inappropriate dispatch minutes";
-			minutes = stoi(partT);
-			if (minutes > MAX_MINUTE_NUMBER_PER_HOUR)
-				throw "Inappropriate dispatch minutes";
-
-			tr.dispatch_time_ = SetTime(hours, minutes);
-			beg = endT + 2;      //Т.к не ' ', a ", "
-			end = size(info) - 1;
-			strTest(beg, end);
-
-
-			part = info.substr(beg, end - beg);
-			endT = info.find_first_of(beg, ':');
-			strTest(beg, end);
-			partT = info.substr(beg, endT - beg);
-			if (!isInt(partT))
-				throw "Inappropriate dispatch hours";
-			hours = stoi(partT);
-
-			begT = ++endT;
-			strTest(beg, end);
-			partT = info.substr(begT, end - begT);
-			if (!isInt(partT))
-				throw "Inappropriate dispatch minutes";
-			minutes = stoi(partT);
-			if (minutes > MAX_MINUTE_NUMBER_PER_HOUR)
-				throw "Inappropriate dispatch minutes";
-
-			tr.travelling_time_ = (hours * 60 + minutes) * 60;
-		}
-		catch (const char* c)
-		{
-			std::cerr << c << '\n';
-		}
-	}
-	in.close();
 }
 std::time_t timeToNumber(std::string time)
 {
@@ -198,46 +99,122 @@ std::time_t intervalToNumber(std::string interval)
 
 	return (hours * 60 + minutes) * 60;
 }
-void Train::myTrain(const Train& tr)
+
+Train::Train() {};
+void readTrainFile(std::vector<Train>& v, const std::string& fileName)
 {
-	std::cout << tr.id_ << ' ' << tr.type_ << ' ' << tr.destination_ << ' ';
-	PrintTime(tr.dispatch_time_);
-	int timeMinute = tr.travelling_time_/60;
+	std::string info;
+	std::string part;
+	std::string partT;
+	size_t beg, begT;
+	size_t end, endT;
+	int hours, minutes;
+	Train tr;
+	std::ifstream in;
+	in.open(fileName);
+	if (!in.is_open())
+	{
+		throw "Input file error";
+	}
+
+	while (!in.eof())
+	{
+		getline(in, info);
+
+		beg = 0;
+		end = info.find_first_of(' ');
+		strTest(beg, end);
+
+		part = info.substr(beg, end - beg);
+		if (!isInt(part))
+			throw "Inappropriate TrainID";
+		if (stoi(part) > 999999)
+			throw "Inappropriate TrainID: limits exceeding";
+		tr.id_ = stoi(part);
+		beg = end+1;
+		end= info.find_first_of(' ', beg);
+		strTest(beg, end);
+
+
+		part = info.substr(beg, end - beg);
+		tr.type_=strTrainType(part);
+		beg = ++end;
+		end = info.find_first_of(' ', beg);
+		strTest(beg, end);
+
+
+		part = info.substr(beg, end - beg);
+		tr.destination_ = part;
+		beg = ++end;
+		end = info.find_first_of(',', beg);
+		strTest(beg, end);
+
+
+		part = info.substr(beg, end - beg);
+		tr.dispatch_time_ = timeToNumber(part);
+		beg = end + 2;      //Рў.Рє РЅРµ ' ', a ", "
+		end = size(info);
+		strTest(beg, end);
+
+
+		part = info.substr(beg, end - beg);
+		tr.travelling_time_ = intervalToNumber(part);
+		v.push_back(tr);
+	}
+	in.close();
+}
+void Train::myTrain()
+{
+	std::string ID;
+	int dif = 6 - size(std::to_string(id_));
+	for (int i = 0; i < dif; ++i)
+		ID+='0';
+	ID += std::to_string(id_);
+	std::cout << ID << ' ' << std::setw(12) << type_ << ' ' << std::setw(15) << destination_ << "   ";
+	PrintTime(dispatch_time_);
+	int timeMinute = travelling_time_/60;
 	int hours = timeMinute / 60;
 	int minutes = timeMinute % 60;
-	std::cout << ", " << hours << ':' << minutes;
+	std::string stH = std::to_string(hours), stM = std::to_string(minutes);
+	if (hours < 10)
+		stH = '0' + stH;
+	if (minutes < 10)
+		stM = '0' + stM;
+
+	std::cout << ",  " << stH << ':' << stM;
+
 }
-void Train::sortByDispTime(std::vector<Train>& v)
+void sortByDispTime(std::vector<Train>& v)
 {
 	std::cout << "Every train sorted by its dispatch time:\n";
 	int size = v.size();
 	for (int i = 0; i < size - 1; ++i)
-		for (int j = i + 1; j < size - 1; ++j)
-			if (v[i].dispatch_time_ < v[i + 1].dispatch_time_)
-				std::swap(v[i], v[i + 1]);
-	for (int i = 0; i < size - 1; ++i)
+		for (int j = 0; j < size - i - 1; ++j)
+			if (v[j].dispatch_time_ > v[j+1].dispatch_time_)
+				std::swap(v[j], v[j + 1]);
+	for (int i = 0; i < size; ++i)
 	{
-		myTrain(v[i]);
+		v[i].myTrain();
 		std::cout << '\n';
 	}
 }
-void Train::displayTrainInfo_timeInterval(std::vector<Train>& v, std::string beginning, std::string ending)
+void displayTrainInfo_timeInterval(std::vector<Train>& v, std::string beginning, std::string ending)
 {
 	std::time_t timeBeg = timeToNumber(beginning), timeEnd = timeToNumber(ending);
-	std::cout << "The trains between " << beginning << " and " << ending << ":\n";
+	std::cout << "The trains between " << beginning << " and " << ending << " dispatch time:\n";
 	int size = v.size();
 	bool thereIs = false;
 	for (int i = 0; i < size; ++i)
 		if (v[i].dispatch_time_ >= timeBeg && v[i].dispatch_time_ <= timeEnd)
 		{
-			myTrain(v[i]);
+			v[i].myTrain();
 			std::cout << '\n';
 			thereIs = true;
 		}
 	if (!thereIs)
 		std::cout << "No such trains\n";
 }
-void Train::displayTrainInfo_destination(std::vector<Train>& v, std::string dest)
+void displayTrainInfo_destination(std::vector<Train>& v, std::string dest)
 {
 	std::cout << "The trains to " << dest << ":\n";
 	int size = v.size();
@@ -245,14 +222,14 @@ void Train::displayTrainInfo_destination(std::vector<Train>& v, std::string dest
 	for (int i = 0; i < size; ++i)
 		if (v[i].destination_ == dest)
 		{
-			myTrain(v[i]);
+			v[i].myTrain();
 			std::cout << '\n';
 			thereIs = true;
 		}
 	if (!thereIs)
 		std::cout << "No such trains\n";
 }
-void Train::displayTrainInfo_destination_type(std::vector<Train>& v, std::string dest, TrainType type)
+void displayTrainInfo_destination_type(std::vector<Train>& v, std::string dest, TrainType type)
 {
 	std::cout << "The "<< type <<" trains to " << dest << ":\n";
 	int size = v.size();
@@ -260,19 +237,19 @@ void Train::displayTrainInfo_destination_type(std::vector<Train>& v, std::string
 	for (int i = 0; i < size; ++i)
 		if (v[i].destination_ == dest && v[i].type_ == type)
 		{
-			myTrain(v[i]);
+			v[i].myTrain();
 			std::cout << '\n';
 			thereIs = true;
 		}
 	if (!thereIs)
 		std::cout << "No such trains\n";
 }
-void Train::displayFastestTrainInfo_destination(std::vector<Train>& v, std::string dest)
+void displayFastestTrainInfo_destination(std::vector<Train>& v, std::string dest)
 {
 	std::cout << "The fastest train to " << dest << ":\n";
 	int size = v.size();
 	std::time_t minTravTime;
-	int index = 0;
+	int index = size;
 	bool thereIs = false;
 	for (int i = 0; i < size; ++i)
 	{
@@ -288,14 +265,18 @@ void Train::displayFastestTrainInfo_destination(std::vector<Train>& v, std::stri
 		}
 	}
 	for (int i = index + 1; i < size; ++i)
-		if (v[i].travelling_time_<minTravTime)
-		{
-			minTravTime = v[i].travelling_time_;
-			index = i;
-		}
+		if (v[i].destination_ == dest)
+			if (v[i].travelling_time_ < minTravTime)
+			{
+				minTravTime = v[i].travelling_time_;
+				index = i;
+			}
 
 	if (thereIs)
-		myTrain(v[index]);
+	{
+		v[index].myTrain();
+		std::cout << '\n';
+	}
 	else
 		std::cout << "No such trains\n";
 }
